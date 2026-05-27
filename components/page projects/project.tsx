7807fab9d8e6cdc146c961e186d8ba2bba5c2cb6
MARIE Théo <theomarie50350@gmail.com>
2026-05-27 22:31:32 +0200

    ajout du lien vers les projets depuis le listing de projets manque les outils utilisé pour les projets dans le listing

--- components/page projects/project.tsx
+++ components/page projects/project.tsx
@@ -26,32 +26,25 @@ interface OutilsProps {
     }[];
 }
 
-interface userProps {
-    id: number;
-    name: string;
-    mdp: string;
-    theme: string;
-    presentation:string;
-    created_at: string;
-}
-
-export default function Project({project, outils, user}: {project: projectProps, outils: OutilsProps[],user:userProps}) {
+export default function Project({project, outils}: {project: projectProps, outils: OutilsProps[]}) {
 
     const id_project = project.id;
 
     return (
-        <Card className="pt-4 mb-4 relative pb-17 gap- hover:shadow-lg transition-shadow duration-300 lg:scale-100 hover:lg:scale-[100.5%] hover:cursor-pointer">
-            <CardHeader>
-                <CardAction>
-                    <Badge variant="outline" className={tools.defineBadgecolor(project.etat.couleur)}>{project.etat.name}</Badge> 
-                    <Badge variant="outline" className={tools.definePublicBadgeColor(project.public)}>{project.public ? 'Public' : 'Privé'}</Badge>
-                </CardAction>
-                <CardTitle className="hover:underline underline-offset-1">{project.title}</CardTitle>
-            </CardHeader>
-            <CardDescription className="pl-6 pr-5">&emsp;{''+project.presentation}</CardDescription>
-            <CardFooter className="absolute bottom-3 flex flex-wrap gap-1">
-                <BadgesOutils props={{id_project,outils}} />
-            </CardFooter>
-        </Card>
+        <a href={`project/${id_project}`} className="h-min mb-4">
+            <Card className="pt-4 relative pb-17 gap- hover:shadow-lg transition-shadow duration-300 lg:scale-100 hover:lg:scale-[100.5%] hover:cursor-pointer">
+                <CardHeader>
+                    <CardAction>
+                        <Badge variant="outline" className={tools.defineBadgecolor(project.etat.couleur)}>{project.etat.name}</Badge> 
+                        <Badge variant="outline" className={tools.definePublicBadgeColor(project.public)}>{project.public ? 'Public' : 'Privé'}</Badge>
+                    </CardAction>
+                    <CardTitle className="hover:underline underline-offset-1">{project.title}</CardTitle>
+                </CardHeader>
+                <CardDescription className="pl-6 pr-5">&emsp;{''+project.presentation}</CardDescription>
+                <CardFooter className="absolute bottom-3 flex flex-wrap gap-1">
+                    <BadgesOutils props={{id_project,outils}} />
+                </CardFooter>
+            </Card>
+        </a>
     );
 }
